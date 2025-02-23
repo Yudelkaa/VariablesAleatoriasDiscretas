feb719ef1574b33de6a49835741c75c3c8122538
Yudelka Elister Guillén Durán <yudelka_guillen@ucne.edu.do>
2025-02-23 17:18:19 -0400

    Add Habitacion class

---
+++ Habitacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulacion
+{
+    public class Habitacion
+    {
+        public int Numero { get; set; }
+        public string Tipo { get; set; } // Económica, Estándar, Lujo
+        public string Tamano { get; set; } // Pequeña, Mediana, Grande
+        public int Camas { get; set; }
+        public int VecesOcupada { get; set; } = 0;
+    }
+}
