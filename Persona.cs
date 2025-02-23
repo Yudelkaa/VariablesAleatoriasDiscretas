d23555fafafcbc6ac16bbe8fb1607732d25f41c9
Yudelka Elister Guillén Durán <yudelka_guillen@ucne.edu.do>
2025-02-23 17:18:02 -0400

    Add Persona class

---
+++ Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulacion
+{
+    public class Persona
+    {
+        public int GrupoPersonas { get; set; }
+        public string PreferenciaTamano { get; set; }
+        public string Presupuesto { get; set; }
+
+
+    }
+}
