266045265bf289ad2a668fa41c15711a80a210db
Yudelka Elister Guillén Durán <yudelka_guillen@ucne.edu.do>
2025-02-26 08:13:13 -0400

    Agregar precio, TieneVistaAlMar, CercaAscensor, TieneBalcon y Ocupada

--- Habitacion.cs
+++ Habitacion.cs
@@ -9,9 +9,20 @@ namespace Simulacion
     public class Habitacion
     {
         public int Numero { get; set; }
+        public int Precio { get; set; }
         public string Tipo { get; set; } // Económica, Estándar, Lujo
         public string Tamano { get; set; } // Pequeña, Mediana, Grande
         public int Camas { get; set; }
         public int VecesOcupada { get; set; } = 0;
+        public bool TieneVistaAlMar {get;set;}
+        public bool CercaAscensor{get;set;}
+        public bool TieneBalcon { get;set;}
+        public bool Ocupada { get; set; }
+        public Habitacion(int numero, string tipo, string tamano, int camas, bool vista, bool ascensor, bool balcon)
+        {
+            Numero = numero; Tipo = tipo; Tamano = tamano; Camas = camas;
+            TieneVistaAlMar = vista; CercaAscensor = ascensor; TieneBalcon = balcon;
+        }
+
     }
 }
